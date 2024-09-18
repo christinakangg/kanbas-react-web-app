11b9a090ae480f3fc789193d23c7f08fa53d71f7
christinakangg <christinakang0628@gmail.com>
2024-09-18 17:30:20 -0400

    A1

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,6 +7,7 @@ export default function Labs() {
   return (
     <div>
       <h1>Labs</h1>
+      <h2>Christina Kang, Section 20595</h2>
       <TOC />
       <Routes>
         <Route path="/" element={<Navigate to="Lab1" />} />
