39e6b4ac97112873895164d0d8a5808b6ec1c6da
christinakangg <christinakang0628@gmail.com>
2024-10-27 10:32:44 -0400

    a4

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -9,6 +9,10 @@ import DateStateVariable from "./DateStateVariable";
 import ObjectStateVariable from "./ObjectStateVariable";
 import ArrayStateVariable from "./ArrayStateVariable";
 import ParentStateComponent from "./ParentStateComponent";
+import ReduxExamples from "./ReduxExamples";
+import HelloRedux from "./ReduxExamples/HelloRedux";
+import CounterRedux from "./ReduxExamples/CounterRedux";
+import AddRedux from "./ReduxExamples/AddRedux";
 
 export default function Lab4(){
     function sayHello() {
@@ -29,6 +33,10 @@ export default function Lab4(){
         <ObjectStateVariable />
         <ArrayStateVariable />
         <ParentStateComponent />
+        <ReduxExamples />
+        <HelloRedux />
+        <CounterRedux />
+        <AddRedux />
     </div>
     )
     
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -4,8 +4,11 @@ import TOC from "./TOC";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
 import Lab4 from "./Lab4";
+import store from "./Lab4/store";
+import { Provider } from "react-redux";
 export default function Labs() {
   return (
+    <Provider store={store}>
     <div>
       <h1>Labs</h1>
       <h2>Christina Kang, Section 2</h2>
@@ -18,6 +21,7 @@ export default function Labs() {
         <Route path="Lab4/*" element={<Lab4 />} />
       </Routes>
     </div>
+    </Provider>
   );
 }
 
